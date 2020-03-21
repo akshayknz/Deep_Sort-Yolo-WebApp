f57a901688042af3fe0702bc83b41d1c5098bf4c
akshayknz <akshayakn6@gmail.com>
2020-03-21 15:13:38 +0530

    app nodify

--- pwa/public/firebase-messaging-sw.js.js
+++ pwa/public/firebase-messaging-sw.js.js
@@ -4,10 +4,10 @@ importScripts('https://www.gstatic.com/firebasejs/3.9.0/firebase-messaging.js');
 // Initialize the Firebase app in the service worker by passing in the
 // messagingSenderId.
 firebase.initializeApp({
-   'messagingSenderId': 'YOUR-SENDER-ID',
-   'apiKey': 'YOUR_API_KEY',
-   'projectId': 'YOUR_PROJECT_ID', 
-   'appId': 'YOUR_APP_ID',
+   'messagingSenderId': '272062630249',
+   'apiKey': 'AAAAP1gySWk:APA91bEJogPOykWMPEc4hoFer2QXo0uOijVODd2AHrb0qHsIWFRLAFGEDw-YZfEVi0oC9G_zh2j2dZ4RIvMyJi-cf3kvo2gZMRHN8PFvYDMwzAFVk4qlLmo5NyQNXhZNvzlqP7w6pG20',
+   'projectId': 's8project-test', 
+   'appId': '1:272062630249:web:5dd41ef42e40ce579095f8',
 });
 
 // Retrieve an instance of Firebase Messaging so that it can handle background
