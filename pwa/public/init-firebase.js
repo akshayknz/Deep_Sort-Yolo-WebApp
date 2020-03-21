a08d5ff76d67b8bd9bea0d861bd1c4a5f2791fbd
akshayknz <akshayakn6@gmail.com>
2020-03-21 17:40:50 +0530

    Clinet to server
    Workin database rw from client using firestore

---
+++ pwa/public/init-firebase.js
@@ -0,0 +1,15 @@
+// TODO: Replace the following with your app's Firebase project configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyAkohDEqaC8qAyldVV3P-sBdaLPHWWpO2c",
+    authDomain: "s8project-test.firebaseapp.com",
+    databaseURL: "https://s8project-test.firebaseio.com",
+    projectId: "s8project-test",
+    storageBucket: "s8project-test.appspot.com",
+    messagingSenderId: "272062630249",
+    appId: "1:272062630249:web:5dd41ef42e40ce579095f8",
+    measurementId: "G-53HD6V8H1B"
+  // ...
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
